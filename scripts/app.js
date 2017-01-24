75bc004b5613ef1167f7b2980d5d4bf75a66bf9e
Rachel Sweeney <rachelgsweeney@gmail.com>
2017-01-23 19:06:53 -0700

    add images, fix images fix java

--- scripts/app.js
+++ scripts/app.js
@@ -13,47 +13,47 @@ $(document).ready(function() {
       return evt.target.tagName === "A";
     });
 
-  getWeather();
-
-  function getWeather() {
-    $.ajax({
-      url : "http://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json",
-      dataType : "jsonp",
-      success : function(parsed_json) {
-        var conditions = parsed_json.current_observation.weather;
-        loadImage(conditions);
-      }
-    });
-  }
-
-  function getTimeOfDay() {
-    var time = new Date();
-    var hours = time.getHours();
-    var timeOfDay;
-
-    if(hours > 17) {
-      timeOfDay = "night";
-    } else if (hours > 12) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "morning";
-    }
-
-    return timeOfDay;
-  }
-
-  function loadImage(conditions) {
-    var imageSRC = "img/weather/hero-"
-    var validConditions = ["clear", "cloudy", "rain", "snow"];
-    var timeOfDay = getTimeOfDay();
-    conditions = conditions.toLowerCase();
-
-    if (validConditions.indexOf(conditions) == -1) {
-      conditions = "cloudy";
-    }
-
-    imageSRC = imageSRC + conditions + "-" + timeOfDay + ".jpg";
-    $("#intro").css("background-image", "url("+ imageSRC + ")");
-  }
-
+// getWeather();
+//
+//   function getWeather() {
+//     $.ajax({
+//       url : "http://api.wunderground.com/api/4e1fe0d302980707/geolookup/conditions/q/NY/Manhattan.json",
+//       dataType : "jsonp",
+//       success : function(parsed_json) {
+//         var conditions = parsed_json.current_observation.weather;
+//         loadImage(conditions);
+//       }
+//     });
+//   }
+//
+//   function getTimeOfDay() {
+//     var time = new Date();
+//     var hours = time.getHours();
+//     var timeOfDay;
+//
+//     if(hours > 17) {
+//       timeOfDay = "night";
+//     } else if (hours > 12) {
+//       timeOfDay = "afternoon";
+//     } else {
+//       timeOfDay = "morning";
+//     }
+//
+//     return timeOfDay;
+//   }
+//
+//   function loadImage(conditions) {
+//     var imageSRC = "img/weather/hero-"
+//     var validConditions = ["clear", "cloudy", "rain", "snow"];
+//     var timeOfDay = getTimeOfDay();
+//     conditions = conditions.toLowerCase();
+//
+//     if (validConditions.indexOf(conditions) == -1) {
+//       conditions = "cloudy";
+//     }
+//
+//     imageSRC = imageSRC + conditions + "-" + timeOfDay + ".jpg";
+//     $("#intro").css("background-image", "url("+ imageSRC + ")");
+//   }
+//
 });
